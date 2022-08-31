554c7010abfbe3cdadcdd12cf2fa7de1c7183813
afurlong <afurlong78@gmail.com>
2022-08-31 14:24:18 -0400

    mailer update

--- Routes/Contact.js
+++ Routes/Contact.js
@@ -3,13 +3,14 @@ const nodemailer = require("nodemailer");
 const { contactValidation } = require("../Validation/validation");
 
 router.post("/contact", (req, res) => {
+  // console.log(req.body);
   const { error } = contactValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  // console.log(req.body);
-
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.CONTACT_EMAIL,
       pass: process.env.CONTACT_PASSWORD,
@@ -20,12 +21,12 @@ router.post("/contact", (req, res) => {
     from: req.body.email,
     to: process.env.MY_EMAIL,
     subject: req.body.subject,
-    text: req.body.message,
+    text: `from: ${req.body.email}, message: ${req.body.message}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      // console.log(error);
+      // console.log(error, "error on server");
       res.status(400).json({ success: false, data: error });
     } else {
       // console.log("Email sent" + info.response);
--- Validation/validation.js
+++ Validation/validation.js
@@ -2,9 +2,9 @@ const Joi = require("joi");
 
 const contactValidation = (data) => {
   const schema = Joi.object({
-    email: Joi.string().min(6).required().email(),
-    subject: Joi.string().min(6).required(),
-    message: Joi.string().min(6).required(),
+    email: Joi.string().min(4).required().email(),
+    subject: Joi.string().min(4).required(),
+    message: Joi.string().min(4).required(),
   });
 
   return schema.validate(data);
